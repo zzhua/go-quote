9b3998e345e4b0222b04b12dafccab07bec4eb23
Mark Chenoweth <mark@cheno.net>
2016-04-05 21:23:59 -0500

    Yahoo source working

--- quote.go
+++ quote.go
@@ -1,215 +1,152 @@
 package quote
 
 import (
+	"bytes"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
 	"time"
 )
 
+// Prices - Stucture for historical price data
+type Prices struct {
+	Symbol string      `json:"symbol"`
+	Date   []time.Time `json:"date"`
+	Open   []float64   `json:"open"`
+	High   []float64   `json:"high"`
+	Low    []float64   `json:"low"`
+	Close  []float64   `json:"close"`
+	Volume []float64   `json:"volume"`
+}
+
+// Period - for quote history
+type Period string
+
 const (
-	// Historical time periods:
-	Daily   = "d"
-	Weekly  = "w"
-	Monthly = "m"
-	Yearly  = "y"
+	// Daily time period
+	Daily Period = "d"
+	// Weekly time period
+	Weekly Period = "w"
+	// Monthly time period
+	Monthly Period = "m"
+	// Yearly time period
+	Yearly Period = "y"
 )
 
-// Price type that is used for a single day
-type price struct {
-	Date     time.Time
-	Open     float64
-	High     float64
-	Low      float64
-	Close    float64
-	Volume   float64
-	AdjClose float64
-}
+// ToCSV - convert prices to csv
+func (p *Prices) ToCSV(headerRow, symbolColumn bool) string {
 
-// Price type that is used for historical price data.
-type PriceH struct {
-	Date   []time.Time `json:"date,omitempty"`
-	Open   []float64   `json:"open,omitempty"`
-	High   []float64   `json:"high,omitempty"`
-	Low    []float64   `json:"low,omitempty"`
-	Close  []float64   `json:"close,omitempty"`
-	Volume []float64   `json:"volume,omitempty"`
-	Adj    []float64   `json:"adj,omitempty"`
-}
+	var buffer bytes.Buffer
 
-func (p *PriceH) Bar(at int) string {
-	return fmt.Sprintf("%s,%.2f,%.2f,%.2f,%.2f,%.0f,%.2f", p.Date[at].Format("2006-01-02"), p.Open[at], p.High[at], p.Low[at], p.Close[at], p.Volume[at], p.Adj[at])
-}
-
-// Get historical prices for the stock.
-func GetDailyHistory(symbol string, from, to time.Time, adjustPrice bool) (PriceH, error) {
-	var prices PriceH
-	// Create URL with daily frequency of data.
-	data, err := loadHistoricalPrice(symbol, from, to, Daily)
-	if err != nil {
-		return prices, err
+	if headerRow {
+		if symbolColumn {
+			buffer.WriteString("symbol,")
+		}
+		buffer.WriteString("date,open,high,low,close,volume\n")
 	}
 
-	prices, err = parseHistorical(data, adjustPrice)
-	if err != nil {
-		return prices, err
+	for bar := range p.Close {
+		sc := ""
+		if symbolColumn {
+			sc = p.Symbol + ","
+		}
+		dt := fmt.Sprintf("%d-%02d-%02d", p.Date[bar].Year(), p.Date[bar].Month(), p.Date[bar].Day())
+		str := fmt.Sprintf("%s%s,%.2f,%.2f,%.2f,%.2f,%.0f\n",
+			sc, dt, p.Open[bar], p.High[bar], p.Low[bar], p.Close[bar], p.Volume[bar])
+		buffer.WriteString(str)
 	}
 
-	return prices, nil
+	return buffer.String()
 }
 
-// Get stock price history for number of years backwards.
-func HistoryForYears(symbol string, years int, period string, adjustPrice bool) (PriceH, error) {
-	var prices PriceH
-	duration := time.Duration(int(time.Hour) * 24 * 365 * years)
-	to := time.Now()
-	from := to.Add(-duration)
-
-	data, err := loadHistoricalPrice(symbol, from, to, period)
-	if err != nil {
-		return prices, err
+// ToJSON - convert prices to json
+func (p *Prices) ToJSON(indent bool) string {
+	var j []byte
+	if indent {
+		j, _ = json.MarshalIndent(p, "", "  ")
+	} else {
+		j, _ = json.Marshal(p)
 	}
-
-	prices, err = parseHistorical(data, adjustPrice)
-	if err != nil {
-		return prices, err
-	}
-
-	return prices, nil
+	return string(j)
 }
 
-// Load historical data price.
-func loadHistoricalPrice(symbol string, from, to time.Time, period string) ([][]string, error) {
-	url := stockHistoryURL(symbol, from, to, period)
-	var data [][]string
+// NewYahoo - Yahoo historical prices for a symbol
+func NewYahoo(symbol string, from, to time.Time, period Period, adjustPrice bool) (*Prices, error) {
+
+	prices := &Prices{}
+	prices.Symbol = symbol
+
+	url := fmt.Sprintf(
+		"http://ichart.yahoo.com/table.csv?s=%s&a=%d&b=%d&c=%d&d=%d&e=%d&f=%d&g=%s&ignore=.csv",
+		symbol,
+		from.Month()-1, from.Day(), from.Year(),
+		to.Month()-1, to.Day(), to.Year(),
+		period)
 
 	resp, err := http.Get(url)
 	if err != nil {
-		return data, err
+		return prices, err
 	}
 	defer resp.Body.Close()
 
+	var csvdata [][]string
 	reader := csv.NewReader(resp.Body)
-	data, err = reader.ReadAll()
+	csvdata, err = reader.ReadAll()
 	if err != nil {
-		return data, err
+		return prices, err
 	}
 
-	return data, nil
-}
+	numrows := len(csvdata) - 1
+	prices.Date = make([]time.Time, numrows)
+	prices.Open = make([]float64, numrows)
+	prices.High = make([]float64, numrows)
+	prices.Low = make([]float64, numrows)
+	prices.Close = make([]float64, numrows)
+	prices.Volume = make([]float64, numrows)
 
-// Generate request URL for historicat stock data.
-func stockHistoryURL(symbol string, from, to time.Time, frequency string) string {
+	for row := range csvdata {
 
-	// Historical data URL with params:
-	// s - symbol
-	// a - from month (zero based)
-	// b - from day
-	// c - from year
-	// d - to month (zero based)
-	// e - to day
-	// f - to year
-	// g - period frequence (d - daily, w - weekly, m - monthly, y -yearly)
-	yurl := "http://ichart.yahoo.com/table.csv?s=%s&a=%d&b=%d&c=%d&d=%d&e=%d&f=%d&g=%s&ignore=.csv"
-
-	// From date
-	fMonth := (from.Month() - 1) // Need to subtract 1 because months in query is 0 based.
-	fDay := from.Day()
-	fYear := from.Year()
-	// To date
-	tMonth := (to.Month() - 1)
-	tDay := to.Day()
-	tYear := to.Year()
-
-	url := fmt.Sprintf(
-		yurl,
-		symbol,
-		fMonth,
-		fDay,
-		fYear,
-		tMonth,
-		tDay,
-		tYear,
-		frequency)
-
-	return url
-}
-
-// Parse collection of historical prices.
-func parseHistorical(data [][]string, adjustPrice bool) (PriceH, error) {
-	// This is the list of prices with allocated space. Length of space should
-	// subtracted by 1 because the first row of data is title.
-	dl := len(data) - 1
-	prices := PriceH{}
-	prices.Date = make([]time.Time, dl)
-	prices.Open = make([]float64, dl)
-	prices.High = make([]float64, dl)
-	prices.Low = make([]float64, dl)
-	prices.Close = make([]float64, dl)
-	prices.Volume = make([]float64, dl)
-	prices.Adj = make([]float64, dl)
-
-	// We need to leave the first row, because it contains title of columns.
-	for k, v := range data {
-		if k == 0 {
+		if row == 0 { // skip header
 			continue
 		}
-		// Parse row of data into PriceH type and append it to collection of prices.
-		p, err := parseHistoricalRow(v)
-		if err != nil {
-			return prices, err
-		}
 
-		// dl because we are reversing the returned data
-		// (k - 1) because we remove header from the list so index should be reduced by one.
-		prices.Date[dl-k] = p.Date
+		bar := numrows - row // reverse the order
+
+		// Parse row of data and append it to collection of prices
+
+		d, _ := time.Parse("2006-01-02", csvdata[row][0])
+		o, _ := strconv.ParseFloat(csvdata[row][1], 64)
+		h, _ := strconv.ParseFloat(csvdata[row][2], 64)
+		l, _ := strconv.ParseFloat(csvdata[row][3], 64)
+		c, _ := strconv.ParseFloat(csvdata[row][4], 64)
+		v, _ := strconv.ParseFloat(csvdata[row][5], 64)
+		a, _ := strconv.ParseFloat(csvdata[row][6], 64)
+
+		prices.Date[bar] = d
+		prices.Volume[bar] = v
+
 		if adjustPrice {
-			factor := p.AdjClose / p.Close
-			prices.Open[dl-k] = p.Open * factor
-			prices.High[dl-k] = p.High * factor
-			prices.Low[dl-k] = p.Low * factor
-			prices.Close[dl-k] = p.Close * factor
-			prices.Volume[dl-k] = p.Volume
-			prices.Adj[dl-k] = factor
+			factor := a / c
+			prices.Open[bar] = o * factor
+			prices.High[bar] = h * factor
+			prices.Low[bar] = l * factor
+			prices.Close[bar] = c * factor
 		} else {
-			prices.Open[dl-k] = p.Open
-			prices.High[dl-k] = p.High
-			prices.Low[dl-k] = p.Low
-			prices.Close[dl-k] = p.Close
-			prices.Volume[dl-k] = p.Volume
-			prices.Adj[dl-k] = p.AdjClose
+			prices.Open[bar] = o
+			prices.High[bar] = h
+			prices.Low[bar] = l
+			prices.Close[bar] = c
 		}
 	}
+
 	return prices, nil
 }
 
-// Parse data row that comes from historical data. Data row contains
-// 7 columns:
-// 0 - Date
-// 1 - Open
-// 2 - High
-// 3 - Low
-// 4 - Close
-// 5 - Volume
-// 6 - Adj Close
-// This function will return PriceH type that wraps all these columns.
-func parseHistoricalRow(data []string) (price, error) {
-	p := price{}
-
-	// Parse date.
-	d, err := time.Parse("2006-01-02", data[0])
-	if err != nil {
-		return p, err
-	}
-
-	p.Date = d
-	p.Open, _ = strconv.ParseFloat(data[1], 64)
-	p.High, _ = strconv.ParseFloat(data[2], 64)
-	p.Low, _ = strconv.ParseFloat(data[3], 64)
-	p.Close, _ = strconv.ParseFloat(data[4], 64)
-	p.Volume, _ = strconv.ParseFloat(data[5], 64)
-	p.AdjClose, _ = strconv.ParseFloat(data[6], 64)
-
-	return p, nil
+// NewYahooYears - get Yahoo stock price history for a number of years
+func NewYahooYears(symbol string, years int, period Period, adjustPrice bool) (*Prices, error) {
+	to := time.Now()
+	from := to.Add(-time.Duration(int(time.Hour) * 24 * 365 * years))
+	return NewYahoo(symbol, from, to, period, adjustPrice)
 }
